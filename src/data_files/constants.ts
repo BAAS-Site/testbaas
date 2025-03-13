64d2e2f6f04d63dce5ebb61ce596d4059f4938ce
jtat <j.tat@me.com>
2025-03-12 20:23:35 -0700

    site name update

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -3,12 +3,12 @@ import ogImageSrc from "@images/social.png";
 
 
 export const SITE = {
-  title: "ScrewFast",
-  tagline: "Top-quality Hardware Tools",
-  description: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.",
-  description_short: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs.",
-  url: "https://screwfast.uk",
-  author: "Emil Gulamov",
+  title: "BAAS Dragons",
+  tagline: "",
+  description: "",
+  description_short: "",
+  url: "https://baasdragons.org",
+  author: "BAAS Dragons",
 };
 
 export const SEO = {
